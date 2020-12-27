9e09ff8d8eb8d1395a7495d0ac8851247a71768a
zhengpt <zhengpengtao2014@126.com>
2020-12-27 21:47:27 +0800

    新增数据填充

--- app/src/main/java/com/zzz/weather/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/zzz/weather/ui/place/PlaceAdapter.kt
@@ -1,4 +1,33 @@
 package com.zzz.weather.ui.place
 
-class PlaceAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.zzz.weather.R
+import com.zzz.weather.logic.model.Place
+
+class PlaceAdapter(private val fragment: Fragment, private val placeList: List<Place>): RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val placeName = view.findViewById<TextView>(R.id.placeName)
+        val placeAddress = view.findViewById<TextView>(R.id.placeAddress)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.place_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder) {
+            val place = placeList[position]
+            placeName.text = place.name
+            placeAddress.text = place.address
+        }
+    }
+
+    override fun getItemCount() = placeList.size
 }
--- app/src/main/java/com/zzz/weather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/zzz/weather/ui/place/PlaceFragment.kt
@@ -1,4 +1,63 @@
 package com.zzz.weather.ui.place
 
-class PlaceFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.zzz.weather.R
+
+class PlaceFragment: Fragment() {
+    private lateinit var root: View
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: PlaceAdapter
+    private val viewModel by lazy { ViewModelProvider(this).get(PlaceViewModel::class.java) }
+
+    private lateinit var searchPlaceEdit: EditText
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        root = inflater.inflate(R.layout.fragment_place, container, false)
+        return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        recyclerView = root.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        adapter = PlaceAdapter(this, viewModel.placeList)
+        recyclerView.adapter = adapter
+
+        searchPlaceEdit = root.findViewById(R.id.searchPlaceEdit)
+        searchPlaceEdit.addTextChangedListener {
+            val toString = it.toString()
+            if (toString.isNotEmpty()) {
+                viewModel.searchPlaces(toString)
+            } else {
+
+            }
+        }
+
+        viewModel.placeLiveData.observe(viewLifecycleOwner) {
+            val places = it.getOrNull()
+            if (places != null) {
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(context, "未查询到数据", Toast.LENGTH_SHORT).show()
+                it.exceptionOrNull()?.printStackTrace()
+            }
+        }
+    }
+
 }
